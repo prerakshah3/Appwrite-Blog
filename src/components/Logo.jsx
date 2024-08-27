1d77b5a505f6b5b2be5c9ee354efa2927b352db9
prerak shah <sprerak1135@gmail.com>
2024-08-27 10:59:40 +0530

    add logo file  to code

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      Logo
+      <img 
+        src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+        className="mr-3 h-12"
+        alt="Logo" 
+        />
     </div>
   )
 }
